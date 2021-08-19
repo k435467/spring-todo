9947a23d3b6ece1182d25751b8c1c60e29277888
--replace-all <k435467@gmail.com>
2021-08-19 17:00:07 +0800

    spring initializr

---
+++ SpringTodo/src/test/java/ethan/SpringTodo/SpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ethan.SpringTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
